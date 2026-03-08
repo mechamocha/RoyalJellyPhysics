e2b2c2fce2e6a5015fed17ae5a261bf69836068b
mechamocha <leongtoqsum@gmail.com>
2026-03-09 00:36:20 +0800

    added contact manifold struct

--- headers/NarrowPhase.h
+++ headers/NarrowPhase.h
@@ -1,2 +1,21 @@
 #pragma once
 
+#include "Component.h"
+#include "Vector.h"
+
+namespace RJPhysics
+{
+	struct ContactManifold
+	{
+		Entity a;
+		Entity b;
+
+		Vec2 normal;
+		fpm::q16_16 penetration;
+
+		Vec2 contactPoints[2];
+		int pointCount;	// in case contact has <2 points
+	};
+
+
+}
